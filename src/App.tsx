d05c21ca88a248fa35aab3955a97286eebb67341
SamuelSlavik <samuel.slavik@protonmail.com>
2024-07-26 12:51:29 +0200

    Add styled components Add styled container

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './assets/styles/default.css';
+import {Container} from "./components/styles/Container.styled";
 
 function App() {
 
-
   return (
-    <div>
+    <Container>
       working
-    </div>
+    </Container>
   )
 }
 
