c25b44eb0071d08294ab362bb8e40c04f90afe56
SamuelSlavik <samuel.slavik@protonmail.com>
2024-07-26 14:27:55 +0200

    Add fetching logic fore articles and comments Fixed types naming

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,121 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './assets/styles/default.css';
 import {Container} from "./components/styles/Container.styled";
 import {Article} from "./components/article/Article";
 import {Comment} from "./components/comment/Comment";
 import {Loader} from "./components/loader/Loader";
+import {ArticleModel, CommentModel, LoadingFlags} from "./lib/models";
 
-function App() {
+function App(): JSX.Element {
+  // Loading flag for each object to display the content when it's ready
+  // Doesn't like the idea of having object with three attributes, but according to the assignment, only the app.tsx is responsible for loading data
+  // For better scaling would love to separate these fetches into more components
+  const [loading, setLoading] = useState<LoadingFlags>({
+    article: false,
+    comments: false,
+    moreComments: false
+  })
+
+  // States for article and comments
+  // Due to the using of only single state for comments, additional flag is needed to determine if all comments are loaded
+  const [article, setArticle] = useState<ArticleModel>({
+    author: "",
+    date: "",
+    text: ""
+  })
+  const [comments, setComments] = useState<CommentModel[]>([])
+  const [allCommentsLoaded, setAllCommentsLoaded] = useState<boolean>(false)
+
+  // Load comments
+  // Simulating a delay of 2 seconds (easy counting)
+  const fetchComments = async () => {
+    setLoading((prev) => ({...prev, comments: true}))
+    try {
+      const response = await new Promise<CommentModel[]>(resolve =>
+        setTimeout(() => resolve((window as any).document.__comments), 2000)
+      );
+      setComments(response);
+    } catch (error: any) {
+      // Basic error message
+      // For more advanced project would be good to implement some notification store with useNotification hook
+      // that could provide for example addNotification() function
+      // Example usage instead alert: addNotification(type: "error", message: "Error while fetching.....")
+      alert("Error fetching comments: " + error.message)
+    } finally {
+      setLoading((prev) => ({...prev, comments: false}))
+    }
+  }
+
+  // Load more comments
+  // Simulating a delay of 2 seconds (easy counting)
+  // Appending the new comments to the existing ones and setting the flag to true (due to the single comments state)
+  const fetchMoreComments = async () => {
+    setLoading((prev) => ({...prev, moreComments: true}))
+    try {
+      const response = await new Promise<CommentModel[]>(resolve =>
+        setTimeout(() => resolve((window as any).document.__moreComments), 2000)
+      );
+      setComments((prevComments) => [...prevComments, ...response]);
+      setAllCommentsLoaded(true)
+    } catch (error: any) {
+      // Same basic error message, see the suggestion of error handling in the fetchComments
+      alert("Error fetching comments: " + error.message)
+    } finally {
+      setLoading((prev) => ({...prev, moreComments: false}))
+    }
+  }
+
+  // Load article
+  // Simulating a delay of 2 seconds (easy counting)
+  // chaining the fetchComments after the article is fetched
+  const fetchArticle = async () => {
+    setLoading((prev) => ({...prev, article: true}))
+    try {
+      const response = await new Promise<ArticleModel>(resolve =>
+        setTimeout(() => resolve((window as any).document.__article), 2000)
+      );
+      setArticle(response);
+      fetchComments();
+    } catch (error: any) {
+      // Same basic error message, see the suggestion of error handling in the fetchComments
+      alert("Error fetching article: " + error.message)
+    } finally {
+      setLoading((prev) => ({...prev, article: false}))
+    }
+  }
+
+  // Hook with callback to fetch the article (and then comments) when the app is mounted
+  // No more dependencies to watch needed... I think :)
+  useEffect(() => {
+    fetchArticle()
+  }, []);
 
   return (
     <Container>
-      <Article author={"test"} date={"test"} text={"tes text"}/>
-      <Comment author={"test comment"} text={"test com"} date={" test com date"} />
-      <Loader loading={true}></Loader>
+      {/* Conditionally rendering articles and comments if they are loaded. The loader is just a ternary operator wrapped into single component. */}
+      <Loader loading={loading.article}>
+        <Article
+          author={article?.author}
+          date={article?.date}
+          text={article?.text}
+        />
+      </Loader>
+      <Loader loading={loading.comments}>
+        {comments.map((comment) => (
+          <Comment
+            key={comment.id}
+            author={comment.author}
+            date={comment.date}
+            text={comment.text}
+          />
+        ))}
+      </Loader>
+      {/* Rendering the button only if comments are loaded and hiding it again if all comments are loaded */}
+      {
+        (comments.length > 0 && !allCommentsLoaded) && <button onClick={fetchMoreComments}>Load more comments</button>
+      }
+      <Loader loading={loading.moreComments}></Loader>
+
     </Container>
   )
 }
--- src/lib/models.ts
+++ src/lib/models.ts
@@ -1,11 +1,16 @@
-export type Article = {
+export type ArticleModel = {
   author: string
   date: string
   text: string
 }
-export type Comment = {
+export type CommentModel = {
   id : number
   author: string
   text: string
   date: string
+}
+export type LoadingFlags = {
+  article: boolean
+  comments: boolean
+  moreComments: boolean
 }
